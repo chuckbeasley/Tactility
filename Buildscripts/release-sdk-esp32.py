29fe144103353f7178c8238f6209161fc67f81c1
Chuck Beasley <chuck@chuckbeasley.com>
2026-09-06 04:14:28 -0400

    Fix SDK elf_loader mapping to vendored Libraries/elf_loader
    The managed espressif/elf_loader component was replaced with the vendored
    Libraries/elf_loader during the migration, so the SDK generator must copy
    elf_loader.cmake/linker.lf/license.txt from there instead.

--- Buildscripts/release-sdk-esp32.py
+++ Buildscripts/release-sdk-esp32.py
@@ -91,9 +91,9 @@ def main():
         {'src': 'Libraries/lvgl/src/lv_conf_kconfig.h', 'dst': 'Libraries/lvgl/include/lv_conf.h'},
         {'src': 'Libraries/lvgl/src/**/*.h', 'dst': 'Libraries/lvgl/include/src/'},
         # elf_loader
-        {'src': 'managed_components/espressif__elf_loader/*.cmake', 'dst': 'Libraries/elf_loader/'},
-        {'src': 'managed_components/espressif__elf_loader/*.lf', 'dst': 'Libraries/elf_loader/'},
-        {'src': 'managed_components/espressif__elf_loader/license.txt', 'dst': 'Libraries/elf_loader/'},
+        {'src': 'Libraries/elf_loader/*.cmake', 'dst': 'Libraries/elf_loader/'},
+        {'src': 'Libraries/elf_loader/*.lf', 'dst': 'Libraries/elf_loader/'},
+        {'src': 'Libraries/elf_loader/license.txt', 'dst': 'Libraries/elf_loader/'},
         # minitar
         {'src': 'build/esp-idf/minitar/libminitar.a', 'dst': 'Libraries/minitar/binary/'},
         {'src': 'Libraries/minitar/minitar/minitar.h', 'dst': 'Libraries/minitar/include/'},
